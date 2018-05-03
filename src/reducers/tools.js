0f0e1e1625a30151dd742b06165c1ff5ad89c90a
Kirsten Kwong <kskwong@ualberta.ca>
2018-05-03 15:10:18 -0600

    reducers for data requests

--- src/reducers/tools.js
+++ src/reducers/tools.js
@@ -1,1 +1,26 @@
-import { FETCH_DATA } from "../actions/types";
+import { FETCH_DATA, ADD_REQUEST } from "../actions/types";
+
+const initialState = {
+    tools_list = [],
+    tool_map = [],
+    requests = {}
+}
+
+export default function tools(state=initialState, action) {
+
+    switch (action.type) {
+        case 'SERVER/'+ FETCH_DATA:
+            // ????
+            break;
+
+        case ADD_REQUEST:
+            return {
+                ...state,
+                requests: {...state.requests, [action.payload.uuid]: action.payload.request}
+            }
+
+        default:
+            break;
+    }
+
+}
